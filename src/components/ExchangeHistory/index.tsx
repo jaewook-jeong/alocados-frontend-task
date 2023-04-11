0fdb06dd37f6bcc7e338d36303cd09e9098d97d4
jaewook-jeong <dnrlwo11@gmail.com>
2023-04-11 10:52:27 +0900

    [FIX] 숫자 locale string 처리

--- src/components/ExchangeHistory/index.tsx
+++ src/components/ExchangeHistory/index.tsx
@@ -2,6 +2,7 @@ import Icons from '@assets/svg';
 import * as Styled from './ExchangeHistory.styled';
 import { ExchangeHistory as ExchangeHistoryProps } from 'types/crypto';
 import { CRYPTO_DETAIL } from 'constants/crypto';
+import { memo } from 'react';
 
 const ExchangeHistory = ({ data }: { data: ExchangeHistoryProps }) => {
   const fromInfo = CRYPTO_DETAIL[data.fromId];
@@ -13,16 +14,16 @@ const ExchangeHistory = ({ data }: { data: ExchangeHistoryProps }) => {
       <Styled.AmountWrapper>
         <Styled.CrypAmount>
           <fromInfo.Icon />
-          {`${data.amount} ${fromInfo.currencySymbol}`}
+          {`${data.amount.toLocaleString()} ${fromInfo.currencySymbol}`}
         </Styled.CrypAmount>
         <Icons.Right />
         <Styled.CrypAmount>
           <toInfo.Icon />
-          {`${data.amount * data.rate} ${toInfo.currencySymbol}`}
+          {`${(data.amount * data.rate).toLocaleString()} ${toInfo.currencySymbol}`}
         </Styled.CrypAmount>
       </Styled.AmountWrapper>
     </Styled.LatestHistory>
   );
 };
 
-export default ExchangeHistory;
+export default memo(ExchangeHistory);
